4e7493c1e6a75f4a9eb3ebd1538f402f8b37193e
Wyse- <5454041+Wyse-@users.noreply.github.com>
2020-05-10 19:12:03 +0200

    Added ocelot settings to config generation script & docker-compose

--- .docker/web/generate-config.sh
+++ .docker/web/generate-config.sh
@@ -18,8 +18,8 @@ sed -i -e "10s/''/'localhost'/" ${CONF_FILE}
 sed -i -e "11s|'https://'.SITE_HOST|'http://localhost:8080'|" ${CONF_FILE}
 sed -i -e "13s|/path|/var/www|g" ${CONF_FILE}
 sed -i -e "14s|/path|/var/www|g" ${CONF_FILE}
-sed -i -e "16s|''|'http://localhost:34000'|" ${CONF_FILE}
-sed -i -e "17s|''|'https://localhost:3400'|" ${CONF_FILE}
+sed -i -e "16s|''|'http://localhost:${TRACKER_PORT}'|" ${CONF_FILE}
+sed -i -e "17s|''|'http://localhost:${TRACKER_PORT}'|" ${CONF_FILE}
 
 sed -i -e "38s/localhost/mysql/" ${CONF_FILE}
 sed -i -e "39s/''/'${MYSQL_USER}'/" ${CONF_FILE}
@@ -31,7 +31,11 @@ sed -i -e "50s/.*/    ['host' => 'memcached', 'port' => 11211]/" ${CONF_FILE}
 sed -i -e "54s/localhost/sphinxsearch/" ${CONF_FILE}
 sed -i -e "s/127.0.0.1/sphinxsearch/" ${CONF_FILE}
 
-sed -i -e "63s/false/true/" ${CONF_FILE}
+sed -i -e "63s/false/false/" ${CONF_FILE}
+sed -i -e "64s/localhost/${TRACKER_HOST}/" ${CONF_FILE}
+sed -i -e "65s/2710/${TRACKER_PORT}/" ${CONF_FILE}
+sed -i -e "66s/''/'${TRACKER_SECRET}'/" ${CONF_FILE}
+sed -i -e "67s/''/'${TRACKER_REPORTKEY}'/" ${CONF_FILE}
 
 sed -i -e "83s/false/true/" ${CONF_FILE}
 
